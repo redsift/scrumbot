bec2f1889ce40e4ff6c37aef51f25ecc2b5f706f
Michael Karliner <mike@modern-industry.com>
2017-07-31 16:45:03 +0100

    FIX: Missing file

---
+++ server/installation.js
@@ -0,0 +1,35 @@
+/**
+ * Simple Bot Sift. DAG's 'Slack' node implementation
+ */
+
+'use strict';
+
+var slack = require('./slack.js');
+
+// Entry point for DAG node
+module.exports = function(got) {
+  const inData = got['in'];
+  var results = [];
+  var botToken;
+  // Extract the Slack API token
+  console.log("IDD ", inData)
+  inData.data.forEach(function(d) {
+    if(d.key == 'slack/bot_access_token') {
+      botToken = d.value.toString();
+      console.log("INSTALLED! ", botToken)
+    }
+  });
+
+  // Send out standup call message
+  var installMsg = 'Hello, I\'m <@scrum_bot>, please send me stand-up reports on my private channel when I ask for them.';
+
+  try {
+      results.push(slack.postMessage("#general", installMsg, null, botToken).then(()=>{
+        return null;
+      }));
+  } catch (ex) {
+    console.error('meetingcall: Exception: ', ex);
+  }
+
+  return results;
+};
