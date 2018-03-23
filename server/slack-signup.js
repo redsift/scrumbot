1f4f813ede9ed06edf385a99fdb9d224bcd76ada
Martin Hecher <martin@redsift.io>
2018-03-23 17:49:47 +0000

    re-add slack/team_name extraction

--- server/slack-signup.js
+++ server/slack-signup.js
@@ -9,6 +9,11 @@ module.exports = function (got) {
   let rpcValue = {};
   const inData = got['in'];
   let ret = [];
+  inData.data.map(datum => {
+    if(datum.key === 'slack/team_name') {
+      rpcValue.team_name = datum.value.toString();
+    }
+  });  
   ret.push({name: 'slack_info', key: 'slack-signed-up', value: rpcValue });
   return ret;
 };
