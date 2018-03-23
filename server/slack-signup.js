a5557d4fd4cb673ea29e88522a2b9cd23483f982
Martin Hecher <martin@redsift.io>
2018-03-23 19:12:35 +0000

    add debug output

--- server/slack-signup.js
+++ server/slack-signup.js
@@ -9,11 +9,17 @@ module.exports = function (got) {
   let rpcValue = {};
   const inData = got['in'];
   let ret = [];
+
+  console.log('slack-signup: inData:', JSON.stringify(inData, null, 4));
+
   inData.data.map(datum => {
     if(datum.key === 'slack/team_name') {
       rpcValue.team_name = datum.value.toString();
     }
   });  
   ret.push({name: 'slack_info', key: 'slack-signed-up', value: rpcValue });
+
+  console.log('slack-signup: ret:', JSON.stringify(ret, null, 4));
+  
   return ret;
 };
